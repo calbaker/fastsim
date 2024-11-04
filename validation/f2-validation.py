725b7c5b76f0bb1c30d466f5131dd550e8fd9dbb
Kyle Carow <kylecarow@gmail.com>
2024-11-04 10:11:21 -0700

    f3 vehicles converted

--- validation/f2-validation.py
+++ validation/f2-validation.py
@@ -1,6 +1,11 @@
 # Script for validation against fastsim-2
 
+# %%
 from pathlib import Path
+import fastsim
 
-for veh_file in (Path() / "f2-vehicles").iterdir():
-        
+for veh_file in Path("./f2-vehicles").iterdir():
+    print(veh_file)
+    fastsim.Vehicle.from_file(veh_file)
+
+# %%
