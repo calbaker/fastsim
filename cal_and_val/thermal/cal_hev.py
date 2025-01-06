9a6dc72f207d773a523c2993eee92f4c20a85cf8
Chad Baker <chad.baker@nrel.gov>
2025-01-06 16:42:24 -0700

    first commit of calibration scripts

---
+++ cal_and_val/thermal/cal_hev.py
@@ -0,0 +1,6 @@
+# # TODO Calibration Tasks
+# - [ ] put vehicle test data in a sharepoint folder, grant Robin access, and paste link in here
+# - [ ] develop means of skewing curves via setter or similar
+# - [ ] use `.` notation to define parameters and show Robin his this might look in the code
+# - [ ] show what signals should be use for objectives and how to access them in code
+# - [ ] have Robin start running calibration and play with things in the meantime
