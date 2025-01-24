e5834a78de8171623995e1b965761334dcdb58ab
Kyle Carow <kylecarow@gmail.com>
2025-01-24 12:10:36 -0700

    started cal_bev.py

---
+++ cal_and_val/thermal/cal_bev.py
@@ -0,0 +1,10 @@
+"""
+Calibration script for 2020 Chevrolet Bolt EV    
+"""
+
+# critical import
+from pathlib import Path
+
+import fastsim as fsim
+
+# cyc_folder_path = Path(__file__) / "dyno_test_data/2020 Chevrolet Bolt EV/Extended Datasets"
