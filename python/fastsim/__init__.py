b767032ddacc9bd18af89dd7da570bce64d1bfa7
Chad Baker <chad.baker@nrel.gov>
2025-02-10 13:27:57 -0700

    all tests pass

--- python/fastsim/__init__.py
+++ python/fastsim/__init__.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from typing import Any, List, Union, Dict 
 from typing_extensions import Self
 import numpy as np
-import re
 import inspect
 import pandas as pd  # type: ignore[import-untyped]
 import polars as pl
@@ -11,6 +10,7 @@ import polars as pl
 import fastsim
 from .fastsim import *  # noqa: F403
 from .fastsim import Cycle  # type: ignore[attr-defined]
+from . import utils # type: ignore[attr-defined]
 
 DEFAULT_LOGGING_CONFIG = dict(
     format="%(asctime)s.%(msecs)03d | %(filename)s:%(lineno)s | %(levelname)s: %(message)s",
@@ -32,65 +32,6 @@ def resources_root() -> Path:
 
 
 __version__ = version("fastsim")
-
-
-def set_param_from_path(
-    model: Any,
-    path: str,
-    value: Any,
-) -> Any:
-    """
-    Set parameter `value` on `model` for `path` to parameter
-
-    Example usage:
-    todo
-    """
-    path_list = path.split(".")
-
-    def _get_list(path_elem, container):
-        list_match = re.match(r"([\w\d]+)\[(\d+)\]", path_elem)
-        if list_match is not None:
-            list_name = list_match.group(1)
-            index = int(list_match.group(2))
-            lst = container.__getattribute__(list_name).tolist()
-            return lst, list_name, index
-        else:
-            return None, None, None
-
-    containers = [model]
-    lists = [(None, None, None)] * len(path_list)
-    for i, path_elem in enumerate(path_list):
-        container = containers[-1]
-
-        list_attr, list_name, list_index = _get_list(path_elem, container)
-        if list_attr is not None:
-            attr = list_attr[list_index]
-            # save for when we repack the containers
-            lists[i] = (list_attr, list_name, list_index)
-        else:
-            attr = container.__getattribute__(path_elem)
-
-        if i < len(path_list) - 1:
-            containers.append(attr)
-
-    prev_container = value
-
-    # iterate through remaining containers, inner to outer
-    for list_tuple, container, path_elem in zip(
-        lists[-1::-1], containers[-1::-1], path_list[-1::-1]
-    ):
-        if list_tuple is not None:
-            list_attr, list_name, list_index = list_tuple
-            list_attr[list_index] = prev_container
-
-            container.__setattr__(list_name, list_attr)
-        else:
-            container.__setattr__(f"__{path_elem}", prev_container)
-
-        prev_container = container
-
-    return model
-
 
 def __array__(self):
     return np.array(self.tolist())
@@ -190,12 +131,6 @@ def from_pydict(cls, pydict: Dict, data_fmt: str = "msg_pack", skip_init: bool =
     return obj
 
 
-def is_cyc_key(k):
-    is_cyc_key = any(
-        cyc_key for cyc_key in CYC_KEYS if cyc_key == k.split(".")[-1]) and "cyc" in k
-    return is_cyc_key
-
-
 def to_dataframe(self, pandas: bool = False, allow_partial: bool = False) -> Union[pd.DataFrame, pl.DataFrame]:
     """
     Returns time series results from fastsim object as a Polars or Pandas dataframe.
@@ -207,13 +142,15 @@ def to_dataframe(self, pandas: bool = False, allow_partial: bool = False) -> Uni
     obj_dict = self.to_pydict(flatten=True)
     history_dict: Dict[str, Any] = {}
 
-    if len(history_dict) > 0:
-        val0 = next(iter(history_dict.values()))
-    else:
-        val0 = None
-    history_keys = ['.history.']
+    history_keys = ['.history.', 'cyc.', '.cyc.']
+    try:
+        time_ach = next(iter(v for (k, v) in obj_dict.items() if 'veh' in k and 'time_seconds' in 'k'))
+    except StopIteration:
+        time_ach = None
     for k, v in obj_dict.items():
-        if is_cyc_key(k) or any(hk in k for hk in history_keys) or (val0 is not None and len(v) == len(val0)):
+        same_len_as_time_ach = (False if (time_ach is None) else (len(v) == len(time_ach)))
+        hk_in_k = any(hk in k for hk in history_keys)
+        if (hk_in_k or same_len_as_time_ach) and ("__len__" in dir(v)):
             history_dict[k] = v
 
     if allow_partial:
@@ -231,13 +168,13 @@ def to_dataframe(self, pandas: bool = False, allow_partial: bool = False) -> Uni
                 df = pl.DataFrame(history_dict)
             except Exception as err:
                 raise Exception(
-                    f"{err}\nTry passing `allow_partial=True` to `to_dataframe`")
+                    f"{err}\nTry passing `allow_partial=True` to `to_dataframe` or checking for consistent save intervals")
         else:
             try:
                 df = pd.DataFrame(history_dict)
             except Exception as err:
                 raise Exception(
-                    f"{err}\nTry passing `allow_partial=True` to `to_dataframe`")
+                    f"{err}\nTry passing `allow_partial=True` to `to_dataframe` or checking for consistent save intervals")
     return df
 
 
